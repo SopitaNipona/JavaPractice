939dd61bd0ad01eb2f6775a89a280f090757f28d
SopitaNipona <a01748692@tec.mx>
2024-02-09 23:56:20 -0600

    update 2

--- first/OOPintro/src/Human.java
+++ first/OOPintro/src/Human.java
@@ -1,5 +1,22 @@
 public class Human {
-    Human(String name, int age, double weight){
 
+    String name;
+    int age;
+    double weight;
+    Human(String name, int age, double weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    //Constructure overload
+
+    Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void eat(){
+        System.out.println("\"ñam ñam\" said: "+name);
     }
 }
--- first/OOPintro/src/Main.java
+++ first/OOPintro/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Classes
         Car myCar = new Car();
 
         Car myCar2 = new Car();
@@ -11,6 +13,20 @@ public class Main {
         myCar.brake();
         myCar2.brake();
 
+        //Use of constructures
+
         Human human = new Human("Bob",65,70);
+
+        Human human2 = new Human("Tom",30,80);
+
+        System.out.println(human.age);
+        System.out.println(human2.age);
+        human2.eat();
+
+        //Constructure overload
+
+        Human human3 = new Human("Ethan", 16);
+
+
     }
 }
